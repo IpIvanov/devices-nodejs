32822c26157f476d6379bc459f4454cc4df8254d
Ivan Ivanov <ipivanov11@gmail.com>
2018-06-20 16:46:08 +0300

    added devices

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router();
+const path = require('path');
 const PORT = process.env.PORT || 5000
 
 express()
@@ -7,4 +10,29 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api', router);
+
+const MongoClient = require('mongodb').MongoClient;
+const mongoURL = MONGODB_URI;
+
+
+router.get('/devices', function (request, response) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    if (err) throw err;
+    db
+      .collection('devices')
+      .find({})
+      .toArray(function (err, result) {
+        if (err) throw err;
+        response.json(result);
+        db.close();
+      });
+  });
+});
+
